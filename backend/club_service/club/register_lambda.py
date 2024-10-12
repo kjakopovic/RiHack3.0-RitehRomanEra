6e46d8565c930df23096824557a5dcb110f1c179
dini777 <karlojakopov@gmail.com>
2024-10-12 13:31:15 +0200

    Added club register

--- backend/club_service/club/register_lambda.py
+++ backend/club_service/club/register_lambda.py
@@ -17,6 +17,10 @@ def lambda_handler(event, context):
     try:
         email = event['email']
         password = event['password']
+        club_name = event['club_name']
+        tags = event['tags']
+        default_working_hours = event['default_working_hours']
+        working_days = event['working_days']
     except Exception as e:
         return {
             'statusCode': 400,
@@ -27,7 +31,86 @@ def lambda_handler(event, context):
                 'message': f'{e} is missing, please check and try again'
             })
         }
-  
+    
+    if (not isinstance(tags, list)) or len(tags) == 0:
+        return {
+            'statusCode': 400,
+            'headers': {
+                'Content-Type': 'application/json'
+            },
+            'body': json.dumps({
+                'message': 'Tags is missing, please check and try again'
+            })
+        }
+    
+    logger.info(f'REGISTER CLUB - Getting table for clubs.')
+
+    dynamodb = boto3.resource('dynamodb')
+    clubs_table = dynamodb.Table(os.getenv('CLUBS_TABLE_NAME'))
+
+    logger.info(f'REGISTER CLUB - Checking if club already exists.')
+
+    # Getting club by email
+    try:
+        response = clubs_table.get_item(
+            Key={
+                'club_id': email
+            }
+        )
+
+        # Check if club already exists
+        if response.get('Item'):
+            return {
+                'statusCode': 400,
+                'headers': {
+                    'Content-Type': 'application/json'
+                },
+                'body': json.dumps({
+                    'message': 'Club with this email already exists. Do you want to login instead?'
+                })
+            }
+    except Exception as e:
+        logger.error(f'REGISTER CLUB - Unable to read item: {str(e)}')
+
+        return {
+            'statusCode': 500,
+            'headers': {
+                'Content-Type': 'application/json'
+            },
+            'body': json.dumps({
+                'message': f"Unable to read item: {str(e)}"
+            })
+        }
+    
+    logger.info(f'REGISTER CLUB - Adding club to the table.')
+
+    # Add new club to the table
+    try:
+        salt = bcrypt.gensalt(rounds=5)
+        hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt).decode('utf-8')
+        clubs_table.put_item(
+            Item={
+                'club_id': email,
+                'password': hashed_password,
+                'club_name': club_name,
+                'tags': tags,
+                'default_working_hours': default_working_hours,
+                'working_days': working_days
+            }
+        )
+    except Exception as e:
+        logger.error(f'REGISTER USER - Unable to add item: {str(e)}')
+
+        return {
+            'statusCode': 500,
+            'headers': {
+                'Content-Type': 'application/json'
+            },
+            'body': json.dumps({
+                'message': f"Unable to add item: {str(e)}"
+            })
+        }
+
     return {
         'statusCode': 200,
         'headers': {
