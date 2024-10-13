56d6769f3d08d1d1a20319b9d327a1a7debdc5df
dini777 <karlojakopov@gmail.com>
2024-10-13 03:35:58 +0200

    get userse events finished

---
+++ backend/events_service/events/get_users_events_lambda.py
@@ -0,0 +1,87 @@
+import json
+import logging
+import boto3
+import os
+
+import backend.common.common as common_handler
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+def lambda_handler(event, context):
+    try:
+        error_response, email = common_handler.check_is_user_authenticated_and_fetch_email_from_jwt(event)
+    
+        if error_response:
+            return error_response
+
+        # Initialize a DynamoDB resource
+        dynamodb = boto3.resource('dynamodb')
+        users_table = dynamodb.Table(os.getenv('USERS_TABLE_NAME'))
+        events_table = dynamodb.Table(os.getenv('EVENTS_TABLE_NAME'))
+
+        # get item from the DynamoDB table
+        try:
+            user_item = users_table.get_item(
+                Key={
+                    'email': email
+                }
+            )
+
+            if 'Item' not in user_item:
+                return {
+                    'statusCode': 400,
+                    'headers': {
+                        'Content-Type': 'application/json'
+                    },
+                    'body': json.dumps({
+                        'message': 'User not found'
+                    })
+                }
+            
+            event_ids = user_item['Item'].get('events', [])
+
+            events = []
+
+            for id in event_ids:
+                event_item = events_table.get_item(
+                    Key={
+                        'event_id': id
+                    }
+                )
+
+                if 'Item' in event_item:
+                    events.append(event_item['Item'])
+        except Exception as e:
+            logger.error(f'Error saving event to DynamoDB: {str(e)}')
+
+            return {
+                'statusCode': 500,
+                'headers': {
+                    'Content-Type': 'application/json'
+                },
+                'body': json.dumps({
+                    'message': 'Failed to join giveaway. Please try again or contact support.'
+                })
+            }
+
+        return {
+            'statusCode': 200,
+            'headers': {
+                'Content-Type': 'application/json'
+            },
+            'body': json.dumps({
+                'message': 'Joined giveaway successfully!',
+                'events': events
+            })
+        }
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'headers': {
+                'Content-Type': 'application/json'
+            },
+            'body': json.dumps({
+                'message': f"An error occurred: {str(e)}"
+            })
+        }
